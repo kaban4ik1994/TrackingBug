703e34832f70fe5287e03e564a561bd0f1036108
kaban4ik1994 <kaban4ik19901990@mail.ru>
2014-07-21 16:48:55 +0300

    Revert "11.07.2014"
    This reverts commit 99f11b6dc5599864c034bd310820dd8582d5086c.

---
+++ api/Models/BugContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace api.Models
+{
+    public class BugContext: DbContext
+    {
+        public DbSet<Bug> Bugs { get; set; }
+    }
+}
---
+++ api/Scripts/bug.js
@@ -0,0 +1,15 @@
+﻿
+var url = 'http://localhost:52998/api/values';
+
+function GetAllBugs($scope, $http) {
+    $http({ method: 'GET', url: url }).success(function (data) {
+        $scope.bugs = data;
+    });
+}
+
+function DeleteBug($scope, $http, id) {
+    $http({ method: 'DELETE', url: url + id }).success(function (data) {
+        $scope.bugs = data;
+    });
+}
+
